58f5d5c826c32e29d6db788f64a4f46bc21fbab8
Tiran Chanuka <tiranchanuka1998@gmail.com>
2022-04-26 16:11:58 +0530

    Try to add countDown

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Nav from './componet/Nav/Nav';
 import Slide from './componet/Slide/Slide'
+import Event from './componet/Event/event'
 
 function App() {
   return (
     <div>
         <Nav/>
-        <Slide/>
+      <Slide />
+      <Event/>
     </div>
   );
 }
