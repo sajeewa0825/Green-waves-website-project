e23db1cab6f7ce94e136c13402f72f82f116b72f
Tiran Chanuka <tiranchanuka1998@gmail.com>
2022-05-08 00:27:24 +0530

    Update sec.js

--- src/componet/Event/section/sec.js
+++ src/componet/Event/section/sec.js
@@ -1,43 +1,77 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './sec.css'
-import { Container, Row, Col } from 'react-bootstrap' 
-// import image1 from './image/p1.jpg'
+import image1 from './image/p1.jpg'
 // import image2 from './image/p2.jpg'
 
 
 function App() {
     return (
-        <div>
-            <section className="sec">
-            <Container>  
-  <Row className="raw">  
-    <Col className="photo"md={4}></Col>  
-              <Col className="dateTime" md={3}>
-                <p>Time <br /><br />
-                  Date </p>
-              </Col>  
-    <Col className="venue">Discription</Col>  
-            </Row> 
-            
-            <Row className="raw">  
-    <Col className="photo"md={4}></Col>  
-              <Col className="dateTime" md={3}>
-                <p>Time <br /><br />
-                  Date </p>
-              </Col>  
-    <Col className="venue">Discription</Col>  
-            </Row> 
-            <Row className="raw">  
-    <Col className="photo"md={4}></Col>  
-              <Col className="dateTime" md={3}>
-                <p>Time <br /><br />
-                  Date </p>
-              </Col>  
-    <Col className="venue">Discription</Col>  
-            </Row> 
-</Container>
-        </section>
+      <div>
+   <div class="card mb-3" style={{width: '1380px',height:'350px'}}>
+  <div class="row g-0">
+    <div class="col-md-8">
+      <img src={image1} class="img-fluid rounded-start" alt="..."/>
+    </div>
+    <div class="col-md-4">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <p class="card-text"><small class="text-muted">
+   <ol class="list-group list-group-numbered ">
+  <li class="list-group-item d-flex justify-content-between align-items-start">
+    <div class="ms-2 me-auto">
+      <div class="fw-bold">Subheading</div>
+      Content for list item
+    </div>
+    <span class="badge bg-primary rounded-pill">14</span>
+  </li>
+  <li class="list-group-item d-flex justify-content-between align-items-start">
+    <div class="ms-2 me-auto">
+      <div class="fw-bold">Subheading</div>
+      Content for list item
+    </div>
+    <span class="badge bg-primary rounded-pill">14</span>
+  </li>
+  <li class="list-group-item d-flex justify-content-between align-items-start">
+    <div class="ms-2 me-auto">
+      <div class="fw-bold">Subheading</div>
+      Content for list item
+    </div>
+    <span class="badge bg-primary rounded-pill">14</span>
+  </li>
+</ol>
+                </small></p>
+      </div>
+    </div>
+  </div>
+</div>
+        <div class="card-group">
+  <div class="card">
+    <img src={image1} class="card-img-top" alt="..."/>
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div class="card">
+    <img src={image1} class="card-img-top" alt="..."/>
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div class="card">
+    <img src={image1} class="card-img-top" alt="..."/>
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+</div>
         </div>
     );
 }
