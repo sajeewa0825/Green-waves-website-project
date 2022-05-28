04ef7c26284cac058d90c1491d272999a6bb0954
Tiran Chanuka <tiranchanuka1998@gmail.com>
2022-05-28 14:54:38 +0530

    Update doneEvent.js

--- src/componet/section/doneEvent.js
+++ src/componet/section/doneEvent.js
@@ -1,74 +1,56 @@
 import React from "react";
 import "./doneEvent.css";
 import Im1 from "./image/forest1.jpg";
+import reportWebVitals from "./../../reportWebVitals";
 
 const donEvent = () => {
   return (
     <div>
-      <header>
-        <div class="curve">
-          <h1>Our Previous Campaings</h1>
-        </div>
-      </header>
-
-      {/* event card create */}
-      <br />
-      <div class="card mb-3" style={{ marginLeft: "3px" }}>
-        <div class="row g-0">
-          <div class="col-md-4">
-            <img src={Im1} class="img-fluid rounded-start" alt="..." />
-          </div>
-          <div class="col-md-8">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </p>
-              <p class="card-text">
-                <small class="text-muted">Last updated 3 mins ago</small>
-              </p>
+      <h1>Events</h1>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-lg-6">
+            <div class="blog-card1">
+              <div class="meta">
+                <div class="photo1"></div>
+                <ul class="details">
+                  <li class="date">Aug. 24, 2015</li>
+                </ul>
+              </div>
+              <div class="description">
+                <h1>Learning to Code</h1>
+                <h2>Opening a door to the future</h2>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad
+                  eum dolorum architecto obcaecati enim dicta praesentium, quam
+                  nobis! Neque ad aliquam facilis numquam. Veritatis, sit.
+                </p>
+                <p class="read-more">
+                  <a href="#">Read More</a>
+                </p>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
-      <div class="card mb-3" style={{ marginLeft: "3px" }}>
-        <div class="row g-0">
-          <div class="col-md-5">
-            <img src={Im1} class="img-fluid rounded-start" alt="..." />
-          </div>
-          <div class="col-md-7">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </p>
-              <p class="card-text">
-                <small class="text-muted">Last updated 3 mins ago</small>
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="card mb-3" style={{ marginLeft: "3px" }}>
-        <div class="row g-0">
-          <div class="col-md-4">
-            <img src={Im1} class="img-fluid rounded-start" alt="..." />
-          </div>
-          <div class="col-md-8">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </p>
-              <p class="card-text">
-                <small class="text-muted">Last updated 3 mins ago</small>
-              </p>
+          <div className="col-lg-6">
+            <div class="blog-card1 alt">
+              <div class="meta">
+                <div class="photo2"></div>
+                <ul class="details">
+                  <li class="date">July. 15, 2015</li>
+                </ul>
+              </div>
+              <div class="description">
+                <h1>Mastering the Language</h1>
+                <h2>Java is not the same as JavaScript</h2>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad
+                  eum dolorum architecto obcaecati enim dicta praesentium, quam
+                  nobis! Neque ad aliquam facilis numquam. Veritatis, sit.
+                </p>
+                <p class="read-more">
+                  <a href="#">Read More</a>
+                </p>
+              </div>
             </div>
           </div>
         </div>
