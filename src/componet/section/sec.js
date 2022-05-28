e530f13681bdc0463349223a3c94acd3dd883ff7
Tiran Chanuka <tiranchanuka1998@gmail.com>
2022-05-28 14:54:46 +0530

    Update sec.js

--- src/componet/section/sec.js
+++ src/componet/section/sec.js
@@ -45,10 +45,10 @@ function App() {
                 </p>
                 <div class="container justify-content-center">
                   <div class="row justify-content-md-center">
-                    <div class="col-sm-12">
+                    <div class="col-lg-9">
                       <EventCard />
                     </div>
-                    <div class="col-sm-4">
+                    <div class="col-lg-4">
                       <Timer />
                     </div>
                   </div>
