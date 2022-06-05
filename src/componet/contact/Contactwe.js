b9ab362d821cc7a5eb52bb5c229ca01f1c6027aa
Chalani Hansika <ict20068@std.uwu.ac.lk>
2022-06-05 23:23:10 +0530

    Create Contactwe.js

---
+++ src/componet/contact/Contactwe.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ContactweSection from "./contactwesection";
+
+export default function Contact() {
+    return (
+        <div>
+            <ContactweSection />
+        </div>
+    );
+
+}
