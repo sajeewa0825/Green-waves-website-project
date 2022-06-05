f145505a49c8002d4ba88e9bfc850d02e2a39057
Chalani Hansika <ict20068@std.uwu.ac.lk>
2022-06-06 00:15:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,50 +1,52 @@
-import './App.css';
+import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Nav from './componet/Nav/Nav';
-import Slide from './componet/Slide/Slide'
-import Card from './componet/Card/Card';
-import { useState, useEffect } from 'react'
-import Event from './componet/Event/event'
-import { Bars } from  'react-loader-spinner'
-import Footer from './componet/Footer/Footer';
-import CountDown from './componet/Timer/Timer';
-import AboutCard from './componet/AboutCard/AboutCard';
-import Wedo from './componet/WeDoSections/Wedo';
-import BlogCard from './componet/BlogCard/BlogCard';
-
+import Nav from "./componet/Nav/Nav";
+import Slide from "./componet/Slide/Slide";
+import Card from "./componet/Card/Card";
+import { useState, useEffect } from "react";
+import Event from "./componet/Event/event";
+import { Bars } from "react-loader-spinner";
+import Footer from "./componet/Footer/Footer";
+import CountDown from "./componet/Timer/timer";
+import AboutCard from "./componet/AboutCard/AboutCard";
+import Wedo from "./componet/WeDoSections/Wedo";
+import BlogCard from "./componet/BlogCard/BlogCard";
+import ContactUs from "./componet/ContactUs/ContactUs";
 
 function App() {
-
-
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
-    setLoading(true)
+    setLoading(true);
     setTimeout(() => {
-      setLoading(false)
-
-    }, 2000)
-  }, [])
-
+      setLoading(false);
+    }, 2000);
+  }, []);
 
   return (
-    <div className='App' >
-      {
-        loading ?
-          <div className='spiner'>
-            <Bars heigth="100" width="2000" color="green" ariaLabel="loading-indicator" className="spiner" />
-          </div>
-          :
-          <div>
-            <Nav />
-            <Slide />
-            <AboutCard/>
-            <Wedo/>
-            <Card />
-            <BlogCard/>
-            <CountDown/>
-            <Footer/>
-          </div>
-      }
+    <div className="App">
+      {loading ? (
+        <div className="spiner">
+          <Bars
+            heigth="100"
+            width="2000"
+            color="green"
+            ariaLabel="loading-indicator"
+            className="spiner"
+          />
+        </div>
+      ) : (
+        <div>
+          <Nav />
+          <Slide />
+          <AboutCard />
+          <Wedo />
+          <Card />
+          <BlogCard />
+          <CountDown />
+          <ContactUs />
+          <Footer />
+        </div>
+      )}
     </div>
   );
 }
