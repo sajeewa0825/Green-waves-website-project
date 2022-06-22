b7c35579899b592a8c807be027eeb72e032a1f61
Chalani Hansika <ict20068@std.uwu.ac.lk>
2022-06-22 23:58:00 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,51 +1,53 @@
-import './App.css';
+import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useState, useEffect } from 'react'
-import { Bars } from  'react-loader-spinner'
+import { useState, useEffect } from "react";
+import { Bars } from "react-loader-spinner";
 
-import Nav from './componet/Nav/Nav';
-import Footer from './componet/Footer/Footer';
-import Home from './componet/Homepage/Home';
-import Event from './componet/section/sec'
-import WhatWeDo from './componet/WhatWeDo/WhatWedo';
-import OurTeam from './componet/OurTeam/OurTeam';
-
+import Nav from "./componet/Nav/Nav";
+import Footer from "./componet/Footer/Footer";
+import Home from "./componet/Homepage/Home";
+import Event from "./componet/section/sec";
+import WhatWeDo from "./componet/WhatWeDo/WhatWedo";
+import OurTeam from "./componet/OurTeam/OurTeam";
+import ContactUs from "./componet/ContactUs/ContactUs";
 
 function App() {
-
-
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
-    setLoading(true)
+    setLoading(true);
     setTimeout(() => {
-      setLoading(false)
-
-    }, 2000)
-  }, [])
-
+      setLoading(false);
+    }, 2000);
+  }, []);
 
   return (
-    <div className='App' >
-      {
-        loading ?
-          <div className='spiner'>
-            <Bars heigth="100" width="2000" color="green" ariaLabel="loading-indicator" className="spiner" />
-          </div>
-          :
-          <div>
-            <BrowserRouter>
-              <Nav />
-              <Routes>
-                <Route path='Time' element={<Event/>} />
-                <Route path='work' element={<WhatWeDo/>} />
-                <Route path='about' element={<OurTeam/>} />
-                <Route path='/' element={<Home />} />
-              </Routes>
-              <Footer/>
-            </BrowserRouter>
-          </div>
-      }
+    <div className="App">
+      {loading ? (
+        <div className="spiner">
+          <Bars
+            heigth="100"
+            width="2000"
+            color="green"
+            ariaLabel="loading-indicator"
+            className="spiner"
+          />
+        </div>
+      ) : (
+        <div>
+          <BrowserRouter>
+            <Nav />
+            <Routes>
+              <Route path="Time" element={<Event />} />
+              <Route path="work" element={<WhatWeDo />} />
+              <Route path="about" element={<OurTeam />} />
+              <Route path="contactUs" element={<ContactUs />} />
+              <Route path="/" element={<Home />} />
+            </Routes>
+            <Footer />
+          </BrowserRouter>
+        </div>
+      )}
     </div>
   );
 }
