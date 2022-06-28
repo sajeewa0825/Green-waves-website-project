b7f274d39d73e6c797926fb6cc9429fef592fd4a
Tiran Chanuka <tiranchanuka1998@gmail.com>
2022-06-29 00:50:41 +0530

    Add blog to nav

--- src/componet/Nav/Nav.js
+++ src/componet/Nav/Nav.js
@@ -1,41 +1,63 @@
-import React from 'react'
-import './Nav.css';
-import { Link } from 'react-router-dom'
+import React from "react";
+import "./Nav.css";
+import { Link } from "react-router-dom";
 
 const Nav = () => {
   return (
-<nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#" >Bootstrap</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          Bootstrap
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav mx-auto ">
-        <li className="nav-item">
-          <Link className='nav-link' to={"/"}>Home</Link>
-        </li>
-        <li className="nav-item">
-          <Link className='nav-link' to={"/Time"}>Event</Link>
-        </li>
-        <li className="nav-item">
-        <Link className='nav-link' to={"/work"}>What</Link>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Blog</a>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to={"/about"}>About us</Link>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Contact</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-  )
-}
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mx-auto ">
+            <li className="nav-item">
+              <Link className="nav-link" to={"/"}>
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to={"/Time"}>
+                Event
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to={"/work"}>
+                What
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to={"/blog"}>
+                Blog
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to={"/about"}>
+                About us
+              </Link>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-export default Nav
+export default Nav;
