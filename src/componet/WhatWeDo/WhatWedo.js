b4244846b22d2ea67cd94debca0e7fb1beeb60de
GT Lakshan <94892809+gtlakshan@users.noreply.github.com>
2022-07-04 23:21:05 +0530

    About us new

--- src/componet/WhatWeDo/WhatWedo.js
+++ src/componet/WhatWeDo/WhatWedo.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Col, Row } from "react-bootstrap";
 import "./whatWedo.css";
 
+<<<<<<< Updated upstream
 const WhatWeDo = () => {
   return (
     <div>
@@ -27,6 +28,36 @@ const WhatWeDo = () => {
                   typeface without relying on
                 </p>
               </div>
+=======
+
+
+const  WhatWeDo = () => {
+    return (
+        <div>
+            <div class=" container whatwedo">
+                <div class="row">
+                    <div class="col WhatWedoContent">
+                        <div>
+                            <p className='wedotopic'>
+                                <span>WHAT WE DO</span>
+                            </p>
+                            <div className='blacklineWedo'></div>
+                            <div >
+                                <h1 className='Whatwedoh1'> We are working <br/>worldwide </h1>
+                            </div>
+                            <div>
+                                <p className='Whatwedolorem'>
+                                    In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate
+                                    form of a document or a typeface without relying on
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div className='WhatWedocard'></div>
+                    </div>
+                </div>
+>>>>>>> Stashed changes
             </div>
           </div>
           <div class="col">
@@ -66,6 +97,7 @@ const WhatWeDo = () => {
                 </p>
               </div>
 
+<<<<<<< Updated upstream
               <div className="col-md-1">
                 <img
                   src="https://firebasestorage.googleapis.com/v0/b/green-waves-f1d66.appspot.com/o/2.png?alt=media&token=e3c9e059-078a-4e18-a5c2-6cb5f13d6b05"
@@ -81,6 +113,49 @@ const WhatWeDo = () => {
                   Asperiores maxime{" "}
                 </p>
               </div>
+=======
+            <div class=" boxGreen">
+                <div className="rowGreen">
+                    <div className="col mainGrid">
+                        <div>
+                            <div >
+                                <br/>
+                                <h1 class= "mainH1">What we do for the environment</h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            
+            <div className="cardMain">
+
+                <div className="contaner" >
+                    <div className="row ">
+                        <div className="col-md-1">
+                            <img src="https://firebasestorage.googleapis.com/v0/b/green-waves-f1d66.appspot.com/o/1.png?alt=media&token=b96de2a8-7ab3-4016-b66d-e81d26196ca9" class= "imgCard"></img>
+                        </div>
+                        <div className="col-md-3">
+                            <h5 class= "card-title" ><b>Build healthy cities</b></h5>
+                            <p className="cardP">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores maxime </p>
+                        </div>
+                        
+                        <div className="col-md-1">
+                            <img src="https://firebasestorage.googleapis.com/v0/b/green-waves-f1d66.appspot.com/o/2.png?alt=media&token=bae85814-9737-416b-b46a-656005cca429" class= "imgCard"></img>
+                        </div>
+                        <div className="col-md-3">
+                            <h5 class= "card-title" ><b>Protect land and water</b></h5>
+                            <p className="cardP">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores maxime </p>
+                        </div>
+
+                        <div className="col-md-1">
+                            <img src="https://firebasestorage.googleapis.com/v0/b/green-waves-f1d66.appspot.com/o/3.png?alt=media&token=dffd11d2-5935-4bef-b2e1-cdf8c97ce69a" class= "imgCard"></img>
+                        </div>
+                        <div className="col-md-3">
+                        <h5 class= "card-title" ><b>Tree plantation</b></h5>
+                            <p className="cardP">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores maxime </p>
+                        </div>
+                        
+                    </div>
+>>>>>>> Stashed changes
 
               <div className="col-md-1">
                 <img
