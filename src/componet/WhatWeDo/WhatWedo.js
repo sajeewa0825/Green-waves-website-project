a48929bdb49f78b12f2d9b17851d38158a7c5884
Tiran Chanuka <tiranchanuka1998@gmail.com>
2022-07-11 23:50:09 +0530

    Update WhatWedo.js

--- src/componet/WhatWeDo/WhatWedo.js
+++ src/componet/WhatWeDo/WhatWedo.js
@@ -5,9 +5,9 @@ import "./whatWedo.css";
 const WhatWeDo = () => {
   return (
     <div>
-      <div class=" boxWhat">
-        <div class="row">
-          <div class="col WhatWedoContent">
+      <div class="container-fluid boxWhat">
+        <div class="row d-flex justify-content-center m-0 p-0">
+          <div class="col-lg-6 WhatWedoContent ">
             <div>
               <p className="wedotopic">
                 <span>WHAT WE DO</span>
@@ -29,14 +29,117 @@ const WhatWeDo = () => {
               </div>
             </div>
           </div>
-          <div class="col">
+
+          <div class="col-lg-4 ourteamCol2">
             <div className="WhatWedocard"></div>
           </div>
         </div>
       </div>
 
-      <div class=" boxGreen">
-        <div className="rowGreen">
+      <div className="container-fluid boxGreen">
+        <div className="row rowGreen">
+          <h1 class="mainH1">What we do for the environment</h1>
+        </div>
+        <div className="row rowGreen">
+          <div className="col-md-4 colgreenbox">
+            <img
+              src="https://firebasestorage.googleapis.com/v0/b/green-waves-f1d66.appspot.com/o/1.png?alt=media&token=b96de2a8-7ab3-4016-b66d-e81d26196ca9"
+              class="imgCard"
+            ></img>
+            <div>
+              <h5 class="card-titlecard-title wedoTitle">
+                <b>Build healthy cities</b>
+              </h5>
+              <p className="cardP">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Asperiores maxime{" "}
+              </p>
+            </div>
+          </div>
+
+          <div className="col-md-4 colgreenbox">
+            <img
+              src="https://firebasestorage.googleapis.com/v0/b/green-waves-f1d66.appspot.com/o/2.png?alt=media&token=bae85814-9737-416b-b46a-656005cca429"
+              class="imgCard"
+            ></img>
+            <div>
+              <h5 class="card-title wedoTitle">
+                <b>Protect land and water</b>
+              </h5>
+              <p className="cardP">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Asperiores maxime{" "}
+              </p>
+            </div>
+          </div>
+          <div className="col-md-4 colgreenbox">
+            <img
+              src="https://firebasestorage.googleapis.com/v0/b/green-waves-f1d66.appspot.com/o/3.png?alt=media&token=dffd11d2-5935-4bef-b2e1-cdf8c97ce69a"
+              class="imgCard"
+            ></img>
+            <div>
+              <h5 class="card-title wedoTitle">
+                <b>Tree plantation</b>
+              </h5>
+              <p className="cardP">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Asperiores maxime{" "}
+              </p>
+            </div>
+          </div>
+
+          <div className="col-md-4 colgreenbox">
+            <img
+              src="https://firebasestorage.googleapis.com/v0/b/green-waves-f1d66.appspot.com/o/4.png?alt=media&token=8b7de3b0-1c32-4824-b6c4-c556568f2d45"
+              class="imgCard"
+            ></img>
+            <div>
+              <h5 class="card-title wedoTitle">
+                <b>Water sustainability</b>
+              </h5>
+              <p className="cardP">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Asperiores maxime{" "}
+              </p>
+            </div>
+          </div>
+
+          <div className="col-md-4 colgreenbox">
+            <img
+              src="https://firebasestorage.googleapis.com/v0/b/green-waves-f1d66.appspot.com/o/5.png?alt=media&token=7057737a-349d-41de-a4df-c11955b0482d"
+              class="imgCard"
+            ></img>
+            <div>
+              <h5 class="card-title wedoTitle">
+                <b>Animal safety</b>
+              </h5>
+              <p className="cardP">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Asperiores maxime{" "}
+              </p>
+            </div>
+          </div>
+
+          <div className="col-md-4 colgreenbox">
+            <img
+              src="https://firebasestorage.googleapis.com/v0/b/green-waves-f1d66.appspot.com/o/6.png?alt=media&token=97645cb4-5b57-4021-a3c3-2a73c70aec1a"
+              class="imgCard"
+            ></img>
+            <div>
+              <h5 class="card-title wedoTitle">
+                <b>Biodiversity</b>
+              </h5>
+              <p className="cardP">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Asperiores maxime{" "}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* <div class="container-fluid boxGreen">
+        <div className="row rowGreen">
           <div className="col mainGrid">
             <div>
               <div>
@@ -48,61 +151,63 @@ const WhatWeDo = () => {
         </div>
 
         <div className="cardMain">
-          <div className="contaner">
+          <div className="container">
             <div className="row ">
-              <div className="col-md-1">
-                <img
-                  src="https://firebasestorage.googleapis.com/v0/b/green-waves-f1d66.appspot.com/o/1.png?alt=media&token=164d7f62-1911-45a7-8fda-dcbd45be8bd3"
-                  class="imgCard"
-                ></img>
-              </div>
-              <div className="col-md-3">
-                <h5 class="card-title">
-                  <b>Build healthy cities</b>
-                </h5>
-                <p className="cardP">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Asperiores maxime{" "}
-                </p>
-              </div>
+              <div className="col colgreenbox">
+                <div className="col-sm-4 d-flex">
+                  <img
+                    src="https://firebasestorage.googleapis.com/v0/b/green-waves-f1d66.appspot.com/o/1.png?alt=media&token=b96de2a8-7ab3-4016-b66d-e81d26196ca9"
+                    class="imgCard"
+                  ></img>
+                  <div>
+                    <h5 class="card-title wedoTitle">
+                      <b>Build healthy cities</b>
+                    </h5>
+                    <p className="cardP">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Asperiores maxime{" "}
+                    </p>
+                  </div>
+                </div>
 
-              <div className="col-md-1">
-                <img
-                  src="https://firebasestorage.googleapis.com/v0/b/green-waves-f1d66.appspot.com/o/2.png?alt=media&token=e3c9e059-078a-4e18-a5c2-6cb5f13d6b05"
-                  class="imgCard"
-                ></img>
-              </div>
-              <div className="col-md-3">
-                <h5 class="card-title">
-                  <b>Protect land and water</b>
-                </h5>
-                <p className="cardP">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Asperiores maxime{" "}
-                </p>
-              </div>
+                <div className="col">
+                  <img
+                    src="https://firebasestorage.googleapis.com/v0/b/green-waves-f1d66.appspot.com/o/2.png?alt=media&token=bae85814-9737-416b-b46a-656005cca429"
+                    class="imgCard"
+                  ></img>
+                </div>
+                <div className="col">
+                  <h5 class="card-title">
+                    <b>Protect land and water</b>
+                  </h5>
+                  <p className="cardP">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Asperiores maxime{" "}
+                  </p>
+                </div>
 
-              <div className="col-md-1">
-                <img
-                  src="https://firebasestorage.googleapis.com/v0/b/green-waves-f1d66.appspot.com/o/3.png?alt=media&token=6c0a2ba5-3d28-49c7-9437-a71f14243e27"
-                  class="imgCard"
-                ></img>
-              </div>
-              <div className="col-md-3">
-                <h5 class="card-title">
-                  <b>Tree plantation</b>
-                </h5>
-                <p className="cardP">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Asperiores maxime{" "}
-                </p>
+                <div className="col">
+                  <img
+                    src="https://firebasestorage.googleapis.com/v0/b/green-waves-f1d66.appspot.com/o/3.png?alt=media&token=dffd11d2-5935-4bef-b2e1-cdf8c97ce69a"
+                    class="imgCard"
+                  ></img>
+                </div>
+                <div className="col">
+                  <h5 class="card-title">
+                    <b>Tree plantation</b>
+                  </h5>
+                  <p className="cardP">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Asperiores maxime{" "}
+                  </p>
+                </div>
               </div>
             </div>
 
             <br />
             <br />
 
-            <div className="row ">
+            <div className="row">
               <div className="col-md-1">
                 <img
                   src="https://firebasestorage.googleapis.com/v0/b/green-waves-f1d66.appspot.com/o/4.png?alt=media&token=8b7de3b0-1c32-4824-b6c4-c556568f2d45"
@@ -153,7 +258,7 @@ const WhatWeDo = () => {
             </div>
           </div>
         </div>
-      </div>
+      </div> */}
 
       <div class=" boxWhatSec"></div>
     </div>
